1d0b3c57e4a743fa958ac9394eb3a1915e0a42b8
Aiman <ahardao1001@g.educaand.es>
2024-02-02 08:14:36 +0100

    c

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -10,8 +10,19 @@ package daw;
  */
 public class Main {
     public static void main(String[] args) {
-        SopaLetra s1 = new SopaLetra();
+        SopaLetra s1 = new SopaLetra(6,6);
         String palabra = "aiman";
-        System.out.println(s1.colocarPalabraHorizontal(palabra));
+        System.out.println("Apartado A-------------------");
+        s1.colocarPalabraHorizontal(5,1,palabra);
+        System.out.println(s1.toString());
+        System.out.println("Apartado B-------------------");
+        s1.colocarPalabraHorizontalInvertida(1,5,palabra);
+        System.out.println(s1.toString());
+        System.out.println("Apartado C-------------------");
+        s1.colocarPalabraVertical(0, 5, palabra);
+        System.out.println(s1.toString());
+        System.out.println("Apartado D--------------------");
+        s1.colocarPalabraVerticalInvertida(5, 0, palabra);
+        System.out.println(s1.toString());
     }
 }
--- src/main/java/daw/SopaLetra.java
+++ src/main/java/daw/SopaLetra.java
@@ -12,11 +12,13 @@ import org.apache.commons.lang3.RandomStringUtils;
  * @author aiman
  */
 public class SopaLetra {
-    private int fila = 6;
-    private int columna = 6;
+    private int fila ;
+    private int columna ;
     private char[][] letras;
     
-    public SopaLetra() {
+    public SopaLetra(int fila,int columna) {
+        this.fila = fila;
+        this.columna = columna;
         this.letras = new char[fila][columna];
         for(int i = 0;i<letras.length;i++){
             for(int j = 0;j<letras[i].length;j++){
@@ -25,30 +27,84 @@ public class SopaLetra {
         }
     }
     
-    public char[][] colocarPalabraHorizontal(String palabra){
-        for(int i = 0;i<letras.length;i++){
-            for(int j = 0;j<letras[i].length;j++){
-                letras[i][j] = palabra.charAt(i);
+    public void colocarPalabraHorizontal(int fi,int colum,String palabra){
+        char letrasPalabra[] = new char[palabra.length()];   
+        for(int i = 0;i<letrasPalabra.length;i++){
+            letrasPalabra[i]=palabra.charAt(i);
+        }
+        for(int i = 0;i<letrasPalabra.length;i++){
+            try{
+                 letras[fi][colum++] = letrasPalabra[i];
+            }catch(IndexOutOfBoundsException iobe){
+                System.out.println("Está fuera del array");
             }
         }
-        return letras;
     }
+    //colocarPalabraHorizontalInvertida,
+    //que recibe una coordenada de la matriz y un String con la palabra y pone la 
+     //palabra horizontalmente, de forma invertida, en la matriz a partir de la coordenada indicada, si se puede.
+    public void colocarPalabraHorizontalInvertida(int fi,int colum,String palabra){
+        char letrasPalabra[] = new char[palabra.length()];   
+        for(int i = 0;i<letrasPalabra.length;i++){
+            letrasPalabra[i]=palabra.charAt(i);
+        }
+        for(int i = 0;i<letrasPalabra.length;i++){
+            try{
+                 letras[fi][colum--] = letrasPalabra[i];
+            }catch(IndexOutOfBoundsException iobe){
+                System.out.println("Está fuera del array");
+            }
+        }
+    }
+    //colocarPalabraVertical,
+    //que recibe una coordenada de la matriz y un String con la palabra y pone 
+    //la palabra verticalmente en la matriz a partir de la coordenada indicada, si se puede.
+    public void colocarPalabraVertical(int fi,int colum,String palabra){
+        char letrasPalabra[] = new char[palabra.length()];   
+        for(int i = 0;i<letrasPalabra.length;i++){
+            letrasPalabra[i]=palabra.charAt(i);
+        }
+        for(int i = 0;i<letrasPalabra.length;i++){
+            try{
+                 letras[fi++][colum] = letrasPalabra[i];
+            }catch(IndexOutOfBoundsException iobe){
+                System.out.println("Está fuera del array");
+            }
+        }
+    }
+    //colocarPalabraVerticalInvertida,
+   //que recibe una coordenada de la matriz y un String con la palabra y pone 
+   //la palabra verticalmente, de forma invertida, en la matriz a partir de la coordenada indicada, si se puede.
+   public void colocarPalabraVerticalInvertida(int fi,int colum,String palabra){
+       char letrasPalabra[] = new char[palabra.length()];   
+        for(int i = 0;i<letrasPalabra.length;i++){
+            letrasPalabra[i]=palabra.charAt(i);
+        }
+        for(int i = 0;i<letrasPalabra.length;i++){
+            try{
+                 letras[fi--][colum] = letrasPalabra[i];
+            }catch(IndexOutOfBoundsException iobe){
+                System.out.println("Está fuera del array");
+            }
+        }
+   }
     public void mostrarSopa(){
-         for(int i = 0;i<letras.length;i++){
+        for(int i = 0;i<letras.length;i++){
             for(int j = 0;j<letras[i].length;j++){
                 System.out.println(letras[i][j]);
             }
+            System.out.println("|");     
         }
     }
 
     
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<letras.length;i++){
-            System.out.println(" ");
-            for(int j = 0;j<letras[i].length;j++){
-                System.out.print("|"+letras[i][j]);
+       StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < letras.length; i++) {
+            System.out.println("");
+            for (int j = 0; j < letras[i].length; j++) {
+                System.out.print(" | " + letras[i][j]);
             }
         }
         return sb.toString();
